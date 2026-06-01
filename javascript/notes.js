3b6618483f8128d16a2499ae6b3ccff99694f8aa
diwaker321 <diwakeracadmic@gmail.com>
2026-06-01 08:16:53 +0530

    javascript code has been pushed

---
+++ javascript/notes.js
@@ -0,0 +1,10 @@
+//intro to javascript.
+//how js play his role in web dev.
+//what is single thread , multi thread
+//what is sync ,async
+//do some code using js
+//how to use js in index.html
+//how js v8 engine works
+//variables in js 
+//how javascript find the variable 
+//shadow effect of variable
