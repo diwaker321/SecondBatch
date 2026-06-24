abb5ce0b125f5ef3d0164ed5dd8ee581dc2f1257
diwaker321 <diwakeracadmic@gmail.com>
2026-06-24 08:23:58 +0530

    code has been pushed

--- javascript/string/app.js
+++ javascript/string/app.js
@@ -26,10 +26,10 @@
 // console.log(firstname.trim().length);
 
 
-let firstname = "diwaker"
+// let firstname = "diwaker"
 // console.log('firstname: ', firstname);
-let lastname = "dwivedi"
-let age =10
+// let lastname = "dwivedi"
+// let age =10
 // let lastname1 ='dwivedi'
 // let lastname2 = `dwivedi` // backtiks -->template literals
 
@@ -94,8 +94,49 @@ let age =10
 // console.log(name.lastIndexOf('a'))
 // console.log(name.at(-1))
 // let subject = " html css javascript html"
-let greet = "hello"
-console.log(greet.repeat(4));
+// let greet = "hello"
+// console.log(greet.repeat(4));
+
+// let otp = '7876'
+// console.log('otp: ', otp.length);
+// console.log(otp.padStart( 22, "*"));
+// console.log(otp.padEnd( 10, "*"));
+// let nname = '        diwaker         '
+// console.log(nname.length);
+// console.log( nname.trimStart().length);
+// console.log( nname.trimEnd().length);
+// console.log( nname.trim().length);
+
+// let a ='1000';
+// let b = '1000';
+
+// in case of string --> these airthmetic operation will work -> * , - , /
+// but in case of addition i.e --> + it will not work 
+
+// '10' + '2' = '102'
+
+// '1000' + '1000' = '10001000'
+// 'diwaker' + 'dwivedi'
+// console.log('diwaker' * ' dwivedi');
+
+
+// 3 + 4 + '10' = 7 + '10'  = 710
+
+// 10 + 20 + 30 + '40' + 50 = 
+// console.log('d'+10 + 20 + 30 + '40' + 50 + 'd');
+// console.log(undefined - 20);
+
+// addition in string / concat in string 
+
+
+
+// console.log(a+b);
+
+
+
+
+
+
 
 
 
@@ -149,3 +190,8 @@ console.log(greet.repeat(4));
 // string concatination  - concat method
 //conversion str to array . split
 // string methods - lower , upper , trim , includes , startswith , ends , indexof , lastindex , substring , replace , slice , repeat
+// padStart , padEnd,
+
+let value = 'diwaker dwivedi'
+console.log(value.split(" "));
+
