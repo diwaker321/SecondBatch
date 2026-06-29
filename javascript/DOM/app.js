91c6879575f878791f761e8de21b4e6177f9b4ef
diwaker321 <diwakeracadmic@gmail.com>
2026-06-29 08:31:07 +0530

    dom code has been pushed

---
+++ javascript/DOM/app.js
@@ -0,0 +1,14 @@
+// what is dom 
+// what is document , nodes
+// how to access dom 
+//query selectors and query selector all
+//what is htmlcollection and nodelist
+// how to change content
+//how to change style
+//how to add class using classlist
+//how to change attributes.
+//how to use get and setattributes
+//how to make new element using createElement
+
+
+
